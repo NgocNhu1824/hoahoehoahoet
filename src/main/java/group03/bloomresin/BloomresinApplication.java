1220ad2aac5c99ef869d076aa35f7a28c8a91551
Nhu <nhuchnce181233@fpt.edu.vn>
2026-08-07 15:10:26 +0700

    refactor: clean SpringBootApplication main method

--- src/main/java/group03/bloomresin/BloomresinApplication.java
+++ src/main/java/group03/bloomresin/BloomresinApplication.java
@@ -2,17 +2,11 @@ package group03.bloomresin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BloomresinApplication {
 
     public static void main(String[] args) {
-        // container
-        ApplicationContext group04 = SpringApplication.run(BloomresinApplication.class, args);
-        for (String s : group04.getBeanDefinitionNames()) {
-            System.out.println(s);
-        }
+        SpringApplication.run(BloomresinApplication.class, args);
     }
-
 }
